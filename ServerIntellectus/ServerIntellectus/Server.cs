943d44a834e50e4463737abe02241034c5ba15d9
Emilio Montiel <emilio.montiel@hotmail.com>
2019-04-03 03:30:07 -0600

    Se agregó el enlaze del socket a la ip local y al puerto designado, además se permitió al socket escuchar conexiones entrantes

--- ServerIntellectus/ServerIntellectus/Server.cs
+++ ServerIntellectus/ServerIntellectus/Server.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,31 @@ namespace ServerIntellectus
     public class Server
     {
         private Socket SocketServer;
+        IPEndPoint localEndPoint;
 
-        public Server(int puerto)
+        public Server(String IP,int puerto)
         {
             SocketServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                localEndPoint = new IPEndPoint(IPAddress.Parse(IP), puerto);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                SocketServer.Bind(localEndPoint);
+                SocketServer.Listen((int)SocketOptionName.MaxConnections);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
         }
     }
 }
