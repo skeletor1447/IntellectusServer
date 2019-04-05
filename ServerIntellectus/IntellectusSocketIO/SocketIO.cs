4e0997ee6c470621ca9de96665b2a5617c3d61dc
Emilio Montiel <emilio.montiel@hotmail.com>
2019-04-05 11:47:26 -0600

    Se agregó el método ReadInt en la clase SocketIO

--- ServerIntellectus/IntellectusSocketIO/SocketIO.cs
+++ ServerIntellectus/IntellectusSocketIO/SocketIO.cs
@@ -1,8 +1,16 @@
 ﻿using System;
+using System.Net.Sockets;
 
 namespace IntellectusSocketIO
 {
     public class SocketIO
     {
+        /// <summary>Envía un entero de 32 bits al socket destino.</summary>
+        /// <param name="socket"> Socket destino, donde se enviará un entero de 32 bits.</param>
+        /// <param name="numero"> Entero que se enviará al socket destino.</param>
+        public static void ReadInt(Socket socket, int numero)
+        {
+            socket.Receive(BitConverter.GetBytes(numero),sizeof(Int32),SocketFlags.None);
+        }
     }
 }
--- ServerIntellectus/ServerIntellectus/Server.cs
+++ ServerIntellectus/ServerIntellectus/Server.cs
@@ -85,7 +85,6 @@ namespace ServerIntellectus
                 {
                     lClientes.Add(cliente);
                 }
-                
             }
         }
     }
